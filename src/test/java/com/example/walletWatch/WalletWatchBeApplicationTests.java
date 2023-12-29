ca1dfbc654d9e0af634d1b652e403ce37faf0d3e
KavinduNilshan <kavindunilshanliyanage@gmail.com>
2023-12-29 18:20:16 +0530

    Initial commit

---
+++ src/test/java/com/example/walletWatch/WalletWatchBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.walletWatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WalletWatchBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
